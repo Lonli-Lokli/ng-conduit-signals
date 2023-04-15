69f97369fe2fc85bdb262ec2c14715b3ed29619a
Lonli-Lokli <lonli.lokli@gmail.com>
2023-04-15 21:58:07 +0100

    add prettier configuration

--- src/app/shared-utils/is-server.ts
+++ src/app/shared-utils/is-server.ts
@@ -1,4 +1,4 @@
-import { isPlatformServer } from "@angular/common";
-import { PLATFORM_ID, inject } from "@angular/core";
+import { isPlatformServer } from '@angular/common';
+import { PLATFORM_ID, inject } from '@angular/core';
 
 export const injectIsServer = () => isPlatformServer(inject(PLATFORM_ID));
